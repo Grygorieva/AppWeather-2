1046f0b9071840bf51d542ae7f0fbf7fed24d561
Grygorieva <109993142+Grygorieva@users.noreply.github.com>
2022-10-04 00:25:52 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,177 @@
+// 1 - day, time, month, day
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
+        <div class="col my-col">
+            <div class="card h-100">
+              <div class="card-body my-card-body">
+                <h5 class="card-title tex-top-card weather-forecast-date">
+                  ${formatDay(forecastDay.dt)}
+                </h5>
+                <img
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
+                  alt=""
+                  width="56px"
+                  class="icon-svg"
+                />
+                <p class="temperatute weather-forecast-temperatures">
+                  <span class="weather-forecast-temperatures-max"> ${Math.round(
+                    forecastDay.temp.max
+                  )}° </span>
+                  <span class="weather-forecast-temperatures-min"> ${Math.round(
+                    forecastDay.temp.min
+                  )}°  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+           `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "dff5c692192605ee5ed7f95b423ae857";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+}
+
+function search(city) {
+  let apiKey = "dff5c692192605ee5ed7f95b423ae857";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+//navigator.geolocation.getCurrentPosition(searchLocation);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#main-form");
+form.addEventListener("submit", handleSubmit);
+
+// 3 - convert C to F
+//let celsius = document.querySelector("#celsius-link");
+//celsius.addEventListener("click", convertToCelsius);
+
+//let fahrenheit = document.querySelector("#fahrenheit-link");
+//fahrenheit.addEventListener("click", convertToFahrenheit);
+
+//function convertToCelsius(event) {
+//  event.preventDefault();
+//  let currentTemperature = document.querySelector("#temperature");
+//  currentTemperature.innerHTML = 19;
+//}
+
+//function convertToFahrenheit(event) {
+//  event.preventDefault();
+//  let currentTemperature = document.querySelector("#temperature");
+//  currentTemperature.innerHTML = 66;
+//}
+
+//function searchLocation(position) {
+//let apiKey = "ff50fec25423356941c07e6dc6faf1f9";
+//  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+//  axios.get(apiUrl).then(displayWeather);
+//}
+
+//function getCurrentLocation(event) {
+//  event.preventDefault();
+//  navigator.geolocation.getCurrentPosition(searchLocation);
+//}
+
+// let currdateElemententTime = new Date();
+// dateElement.innerHTML = formatDate(currentTime);
+
+function searchLocation(position) {
+  let apiKey = "ff50fec25423356941c07e6dc6faf1f9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+navigator.geolocation.getCurrentPosition(searchLocation);
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-btn");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Berlin");
